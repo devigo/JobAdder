85293f21333687d24e2ec1ed29445efc7b031b75
allexmmr <allexmmr@gmail.com>
2018-05-30 19:57:15 +1000

    Implementing more test cases

--- JobAdder.Tests/CandidateServiceTest.cs
+++ JobAdder.Tests/CandidateServiceTest.cs
@@ -1,7 +1,9 @@
 ﻿using JobAdder.Integrations.Models;
 using JobAdder.Integrations.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace JobAdder.Tests
@@ -9,15 +11,23 @@ namespace JobAdder.Tests
     [TestClass]
     public class CandidateServiceTest
     {
-        private readonly CandidateService _service = new CandidateService();
+        private readonly CandidateService _service = new CandidateService(new HttpClient());
 
         [TestMethod]
         public void Method_ListAllCandidatesAsync()
         {
             Task<List<Candidate>> actual = _service.ListAllCandidatesAsync();
 
+            // Check if there is any candidate
             Assert.IsNotNull(actual);
             Assert.IsTrue(actual.Result.Count >= 0);
+
+            // Check if the id is always an integer
+            foreach (Candidate item in actual.Result)
+            {
+                int.TryParse(Convert.ToString(item.CandidateId), out int candidateId);
+                Assert.IsTrue(candidateId > 0);
+            }
         }
     }
 }
--- JobAdder.Tests/JobServiceTest.cs
+++ JobAdder.Tests/JobServiceTest.cs
@@ -1,7 +1,9 @@
 ﻿using JobAdder.Integrations.Models;
 using JobAdder.Integrations.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace JobAdder.Tests
@@ -9,15 +11,23 @@ namespace JobAdder.Tests
     [TestClass]
     public class JobServiceTest
     {
-        private readonly JobService _service = new JobService();
+        private readonly JobService _service = new JobService(new HttpClient());
 
         [TestMethod]
         public void Method_ListAllJobsAsync()
         {
             Task<List<Job>> actual = _service.ListAllJobsAsync();
 
+            // Check if there is any job
             Assert.IsNotNull(actual);
             Assert.IsTrue(actual.Result.Count >= 0);
+
+            // Check if the id is always an integer
+            foreach (Job item in actual.Result)
+            {
+                int.TryParse(Convert.ToString(item.JobId), out int jobId);
+                Assert.IsTrue(jobId > 0);
+            }
         }
     }
 }
